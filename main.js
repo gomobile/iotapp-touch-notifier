509b8e6d78e6c35421b07fd2eadb74aa36e60d9a
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2014-10-14 10:26:07 -0700

    add project files

--- main.js
+++ main.js
@@ -1,64 +1,33 @@
-//Create node.js Server with homepage (Works)
-/*var http = require('http');
-var app = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to the Galileo Node.js Server \n');
-}).listen(1337);
-console.log('Node.js Server running');
+/*jslint node:true,vars:true, unparam:true */
+/*jshint unused:true */
+
+
+/*
+The Touch Notifier Node.js sample application distributed within Intel® XDK IoT Edition under the IoT with Node.js Projects project creation option showcases how to read digital data from a Grover Starter Kit Plus – IoT Intel® Edition Touch Sensor, start a web server and communicate wirelessly using WebSockets.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+Article: https://software.intel.com/en-us/html5/articles/iot-touch-notifier-nodejs-and-html5-samples
 */
-
-//Create node.js with homepage (Works)
-/*var app = require('express')();
-var http = require('http').Server(app);
-
-app.get('/', function(req, res){
-  res.send('<h1>Hello world from Intel Galileo</h1>');
-});
-
-http.listen(1337, function(){
-  console.log('listening on *:1337');
-});*/
-
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.send('<h1>Hello world from Intel Galileo</h1>');
-});
-
-//Attach a 'connection' event handler to the server
-io.on('connection', function(socket){
-    console.log('a user connected');
-    //Emits an event along with a message
-    socket.emit('connected', 'Welcome');
-    
-    //Start watching Sensors connected to Galileo board
-    startSensorWatch(socket);
-    
-    //Attach a 'disconnect' event handler to the socket
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-});
-
-http.listen(1337, function(){
-  console.log('listening on *:1337');
-});
-
-//MRAA Library was installed on the board directly through ssh session
-var mraa = require("mraa");
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //GROVE Kit Shield D6 --> GPIO6
 //GROVE Kit Shield D2 --> GPIO2
-function startSensorWatch(socket){
-    var touch_sensor_value = 0;
-    var last_t_sensor_value;
+function startSensorWatch(socket) {
+    'use strict';
+    var touch_sensor_value = 0, last_t_sensor_value;
+
     //Touch Sensor connected to D2 pin
     var digital_pin_D2 = new mraa.Gpio(2);
     digital_pin_D2.dir(mraa.DIR_IN);
-    
+
     //Buzzer connected to D6
     var digital_pin_D6 = new mraa.Gpio(6);
     digital_pin_D6.dir(mraa.DIR_OUT);
@@ -67,18 +36,50 @@ function startSensorWatch(socket){
 
     digital_pin_D6.write(0);
 
-    setInterval(function(){
-        touch_sensor_value = digital_pin_D2.read()
-        if (touch_sensor_value == 1 && last_t_sensor_value == 0){
+    setInterval(function () {
+        touch_sensor_value = digital_pin_D2.read();
+        if (touch_sensor_value === 1 && last_t_sensor_value === 0) {
             //console.log("Buzz ON!!!");
             socket.emit('message', "present");
             digital_pin_D6.write(touch_sensor_value);
-        }
-        else if (touch_sensor_value == 0 && last_t_sensor_value == 1){
+        } else if (touch_sensor_value === 0 && last_t_sensor_value === 1) {
             //console.log("Buzz OFF!!!");
             //socket.emit('message', "absent");
             digital_pin_D6.write(touch_sensor_value);
         }
         last_t_sensor_value = touch_sensor_value;
-    },500);
+    }, 500);
 }
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    'use strict';
+    res.send('<h1>Hello world from Intel Galileo</h1>');
+});
+
+//Attach a 'connection' event handler to the server
+io.on('connection', function (socket) {
+    'use strict';
+    console.log('a user connected');
+    //Emits an event along with a message
+    socket.emit('connected', 'Welcome');
+
+    //Start watching Sensors connected to Galileo board
+    startSensorWatch(socket);
+
+    //Attach a 'disconnect' event handler to the socket
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(1337, function () {
+    'use strict';
+    console.log('listening on *:1337');
+});
+
+//MRAA Library was installed on the board directly through ssh session
+var mraa = require("mraa");
